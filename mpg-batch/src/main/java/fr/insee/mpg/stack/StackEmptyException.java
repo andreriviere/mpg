f662b294c8801062286b7ed490b2773bc5caeb49
André Rivière <andre.riviere@insee.fr>
2018-06-13 11:32:28 +0200

    Coquille

--- mpg-batch/src/main/java/fr/insee/mpg/stack/StackEmptyException.java
+++ mpg-batch/src/main/java/fr/insee/mpg/stack/StackEmptyException.java
@@ -7,6 +7,6 @@ public class StackEmptyException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public StackEmptyException() {
-		super("No date in MyStack");
+		super("No data in MyStack");
 	}
 }
